bb3da180ab4acffccd7a956edc27cf83bca0fe55
dixita9393S <dixitasarma2@gmail.com>
2024-08-30 11:03:08 +0530

    serviceimpl

--- src/main/java/com/example/main/serviceimpl/StudentServiceImp.java
+++ src/main/java/com/example/main/serviceimpl/StudentServiceImp.java
@@ -1,5 +1,11 @@
 package com.example.main.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.main.entity.Student;
+import com.example.main.repository.StudentRepository;
+import com.example.main.service.StudentService;
+
 public class StudentServiceImp implements StudentService {
 
 	@Autowired
